472c6823ab0811b6847d544e5aba92a110d8a299
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2020-10-30 13:08:55 +0530

    improved javascript

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const plus =document.querySelectorAll('.plus');
-const questions = document.getElementById('questions')
 
-plus.forEach((plus)=>{
-  plus.addEventListener('click',(e)=>{
-    const ans=e.target.parentElement.parentElement.nextElementSibling;
-    if(e.target.parentElement.className =='plus'){ // if we dont give if statement it will work but if user click some else it will error and nothing will work 
-
-      ans.classList.toggle('show');
-   
-      const rotate=e.target;
-      rotate.classList.toggle('rotate');
-    }
-    
+const question = document.querySelectorAll('.question');
+question.forEach((ques)=>{
+  ques.addEventListener('click',(e)=>{
+    const i =ques.lastElementChild.firstElementChild;
+    const ans=ques.nextElementSibling;
+    ans.classList.toggle('show');
+    i.classList.toggle('rotate');
   })
-})
-
+});
